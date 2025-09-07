781298b5ca219feb7d5420f3eeeddef08cff467c
Kai Trentini <trnkai001@myuct.ac.za>
2025-09-07 12:40:48 +0200

    Scaffolded identity

---
+++ mock-test-2-trnkai3-73957558c69d024900aef52aff47434df7a3c371/Areas/Identity/Data/PPA2025MockContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PPA2025Mock.Areas.Identity.Data;
+
+namespace PPA2025Mock.Data;
+
+public class PPA2025MockContext : IdentityDbContext<PPA2025MockUser>
+{
+    public PPA2025MockContext(DbContextOptions<PPA2025MockContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- mock-test-2-trnkai3-73957558c69d024900aef52aff47434df7a3c371/Program.cs
+++ mock-test-2-trnkai3-73957558c69d024900aef52aff47434df7a3c371/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PPA2025Mock.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PPA2025MockContextConnection") ?? throw new InvalidOperationException("Connection string 'PPA2025MockContextConnection' not found.");
+
+builder.Services.AddDbContext<PPA2025MockContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<PPA2025MockUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PPA2025MockContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
